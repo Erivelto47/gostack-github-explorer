0f0eda959d36a161ecf2ca4d4302612d66d77262
Erivelto Muller <erivelto02@hotmail.com>
2021-01-19 20:45:16 -0300

    Add styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
+import Routes from './routes';
+import { BrowserRouter } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
-  );
-}
-
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 export default App;
--- src/pages/dashboard/styles.tsx
+++ src/pages/dashboard/styles.tsx
@@ -1,3 +1,9 @@
 /**
  * Created by erivelto on 28/11/20
  */
+import styled from 'styled-components';
+
+export const Title = styled.h1 `
+  font-size: 48px;
+  color: #3A3A3A;
+`;
