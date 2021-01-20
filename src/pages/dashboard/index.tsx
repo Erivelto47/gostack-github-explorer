08fed8bc320f22a6800ba84f9c9e43a7a7af50e9
Erivelto Muller <erivelto02@hotmail.com>
2021-01-19 22:00:15 -0300

    Create dashboard styles and first styled components

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,8 +1,25 @@
 import React from 'react';
-import { Title } from './styles';
+
+import logoImg from '../../assets/logo.svg'
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no github</Title>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer"/>
+      <Title>Explore repositórios no Github </Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório"/>
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <></>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
